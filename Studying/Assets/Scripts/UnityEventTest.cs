23c5d98defe706849f120d6844760511452dd6b2
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-04-26 14:32:15 +0200

    UnityEvent
    Understanding unityEvent = null and methods.

--- Studying/Assets/Scripts/UnityEventTest.cs
+++ Studying/Assets/Scripts/UnityEventTest.cs
@@ -3,9 +3,11 @@ using UnityEngine.Events;
 
 public class UnityEventTest : MonoBehaviour
 {
+    public event Delegate Event;
     public int test_int;
     public bool test_bool;
     public UnityEvent unityEvent;
+    public UnityEvent unityEvent22;
     public DynamicTest dynamicTest;
     public DynamicTest2 dynamicTest2;
 
@@ -19,6 +21,9 @@ public class UnityEventTest : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.J))
             Dynamic2_Invoke(test_bool);
+
+        if (Input.GetKeyDown(KeyCode.Y))
+            TestTest();
     }
     private void Dynamic_Invoke(int a, bool b)
     {
@@ -31,5 +36,22 @@ public class UnityEventTest : MonoBehaviour
     private void Method_Test()
     {
         print("Method_Test");
+    }
+    private void TestTest()
+    {
+        if (unityEvent22.GetPersistentEventCount() == 0)
+            print("unityEvent is null");
+        unityEvent22.AddListener(Method_Test);
+        unityEvent22.Invoke();
+        unityEvent22.RemoveListener(Method_Test);
+        if (unityEvent22 == null)
+            print("unityEvent is null");
+        unityEvent22.RemoveAllListeners();
+
+        unityEvent22 = null;
+        unityEvent22.AddListener(Method_Test);
+        Event = null;
+        Event += Method_Test;
+        Event.Invoke();
     }
 }
