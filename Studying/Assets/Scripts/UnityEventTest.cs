ed36598f12e5dd55fd13388529e108eac2366f16
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-06-04 21:47:20 +0200

    Problems
    Problema pubblico privato e persistent non persisten... Guardo appunti su studying, delegates...

--- Studying/Assets/Scripts/UnityEventTest.cs
+++ Studying/Assets/Scripts/UnityEventTest.cs
@@ -1,26 +1,55 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class UnityEventTest : MonoBehaviour
 {
+    UnityEvent unityEvent11;
+    public UnityEvent unityEvent22;
+
+    GameObject gameObjec11;
+    public GameObject gameObjec22;
+    public List<GameObject> list;
+
     public event Delegate Event;
     public int test_int;
     public bool test_bool;
-    public UnityEvent unityEvent;
-    public UnityEvent unityEvent22;
     public DynamicTest dynamicTest;
     public DynamicTest2 dynamicTest2;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
-            unityEvent.Invoke();
+        if (unityEvent11 == null)
+            print("unityEvent11 is null");
+        if (unityEvent22== null)
+            print("unityEvent22 is null");
+        if (gameObjec22 == null)
+            print("gameObjec22 is null");
+        if (gameObjec11 == null)
+            print("gameObjec11 is null");
+        if (list == null)
+            print("list is null");
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            list = null;
 
         if (Input.GetKeyDown(KeyCode.K))
             Dynamic_Invoke(test_int, test_bool);
 
         if (Input.GetKeyDown(KeyCode.J))
             Dynamic2_Invoke(test_bool);
+
+        if (Input.GetKeyDown(KeyCode.L))
+            unityEvent11.Invoke();
+
+        if (Input.GetKeyDown(KeyCode.Q))
+            unityEvent22.Invoke(); //  invoke a null UnityEvent does nothing...
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            /*unityEvent22 = null;*/ // if uncomment create error because can't call method froma null reference
+            unityEvent22.AddListener(Method_Test); //  adding a listener to a null UnityEvent does nothing...
+        }
 
         if (Input.GetKeyDown(KeyCode.Y))
             TestTest();
@@ -40,16 +69,19 @@ public class UnityEventTest : MonoBehaviour
     private void TestTest()
     {
         if (unityEvent22.GetPersistentEventCount() == 0)
-            print("unityEvent is null");
+            print("unityEvent is null"); // this will not print because
+
         unityEvent22.AddListener(Method_Test);
         unityEvent22.Invoke();
         unityEvent22.RemoveListener(Method_Test);
+
         if (unityEvent22 == null)
             print("unityEvent is null");
         unityEvent22.RemoveAllListeners();
 
         unityEvent22 = null;
         unityEvent22.AddListener(Method_Test);
+
         Event = null;
         Event += Method_Test;
         Event.Invoke();
