be52719c3f21bf8b3fe65b947423e3363913ebd2
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-07-20 22:04:04 +0200

    Tris - MinMax work very slow
    Depth ? AlphaBeta ?

--- Studying/Assets/Scripts/AI/Tris/GameLogic.cs
+++ Studying/Assets/Scripts/AI/Tris/GameLogic.cs
@@ -3,9 +3,16 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 using System.Collections;
+using Unity.VisualScripting;
 
 public class GameLogic : MonoBehaviour
 {
+    Dictionary<string, int> scores = new Dictionary<string, int>()
+    {
+        {"X", 1},
+        {"O", -1},
+        {"D", 0},
+    };
     public static List<ButtonPosition> buttons = new List<ButtonPosition>();
 
     [SerializeField]
@@ -21,7 +28,9 @@ public class GameLogic : MonoBehaviour
 
     Round currentRound;
     string currentSimbol;
-    Coroutine ai = null;
+    bool cliked;
+    Coroutine aiCoroutine;
+
     enum Round
     {
         A,
@@ -32,20 +41,23 @@ public class GameLogic : MonoBehaviour
     {
         Restart();
     }
-    private void Update()
-    {
-        print(ai == null);
-    }
     public void Click()
     {
-        ButtonPosition buttonScript = EventSystem.current.currentSelectedGameObject.gameObject.GetComponent<ButtonPosition>();
-        buttonScript.SetSimbol(currentSimbol, false);
-        board[buttonScript.raw, buttonScript.column] = currentSimbol;
-        VerifyBoard();
+        if (!cliked)
+        {
+            ButtonPosition buttonScript = EventSystem.current.currentSelectedGameObject.gameObject.GetComponent<ButtonPosition>();
+            buttonScript.SetSimbol(currentSimbol, false);
+            board[buttonScript.raw, buttonScript.column] = currentSimbol;
+            cliked = true;
+            VerifyBoard();
+        }
     }
     public void Restart()
     {
-        foreach(ButtonPosition b in buttons)
+        if (aiCoroutine != null)
+            StopCoroutine(aiCoroutine);
+
+        foreach (ButtonPosition b in buttons)
         {
             b.SetSimbol("", true);
             b.Interactable(true);
@@ -63,18 +75,20 @@ public class GameLogic : MonoBehaviour
                 currentRound = Round.B;
                 currentSimbol = simbolB;
                 roundText.text = $"{simbolB} is playing...";
+                cliked = false;
                 break;
             case Round.B:
+                cliked = true;
                 currentRound = Round.A;
                 currentSimbol = simbolA;
                 roundText.text = $"{simbolA} is playing...";
-                ai = StartCoroutine(AI());
+                aiCoroutine = StartCoroutine(AI());
                 break;
         }
     }
     void VerifyBoard()
     {
-        string status = Winner(board);
+        string status = CheckWinner(board);
 
         if (status == simbolA)
         {
@@ -90,9 +104,9 @@ public class GameLogic : MonoBehaviour
             return;
         }
 
-        else if (status == "Draw")
+        else if (status == "D")
         {
-            roundText.text = status;
+            roundText.text = "Draw";
             return;
         }
 
@@ -103,7 +117,7 @@ public class GameLogic : MonoBehaviour
     IEnumerator AI()
     {
         yield return new WaitForSeconds(1);
-        int bestScore = -10;
+        int bestScore = -100;
         int[] bestMove = new int[2];
 
         for(int i = 0; i < 3; i++)
@@ -113,7 +127,7 @@ public class GameLogic : MonoBehaviour
                 if (board[i, j] == null)
                 {
                     board[i, j] = currentSimbol;
-                    int score = MinMax(board);
+                    int score = MinMax(board, false);
                     board[i, j] = null;
                     
                     if (score > bestScore)
@@ -134,28 +148,66 @@ public class GameLogic : MonoBehaviour
         }
         print($"Ai moved in: {bestMove[0]} {bestMove[1]}");
         VerifyBoard();
-        ai = null;
     }
-
-    // return state score
-    int MinMax(string[,] state)
+    int MinMax(string[,] state, bool isMax)
     {
-        return 0;
-    }
+        string result = CheckWinner(state);
+        print(result);
+        if (result != "")
+            return scores[result];
 
-    string Winner(string[,] stateToCheck)
+        if (isMax)
+        {
+            int bestScore = -100;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == null)
+                    {
+                        board[i, j] = simbolA;
+                        int score = MinMax(board, false);
+                        board[i, j] = null;
+                        bestScore = Mathf.Max(score, bestScore);
+                    }
+                }
+            }
+            return bestScore;
+        }
+        else
+        {
+            int bestScore = 100;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == null)
+                    {
+                        board[i, j] = simbolB;
+                        int score = MinMax(board, true);
+                        board[i, j] = null;
+                        bestScore = Mathf.Min(score, bestScore);
+                    }
+                }
+            }
+            return bestScore;
+        }
+    }
+    string CheckWinner(string[,] stateToCheck)
     {
         int c = 0;
-
         // check raw
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 2; j++)
             {
-                if (stateToCheck[i, j] != null && stateToCheck[i, j] == stateToCheck[i, j + 1])
-                    c++;
-                if (c == 2)
-                    return stateToCheck[i, j];
+                if (stateToCheck[i, j] != null)
+                {
+                    if (stateToCheck[i, j] == stateToCheck[i, j + 1])
+                        c++;
+                    if (c == 2)
+                        return stateToCheck[i, j];
+                }
             }
             c = 0;
         }
@@ -165,10 +217,13 @@ public class GameLogic : MonoBehaviour
         {
             for (int j = 0; j < 2; j++)
             {
-                if (stateToCheck[j, i] != null && stateToCheck[j, i] == stateToCheck[j + 1, i])
-                    c++;
-                if (c == 2)
-                    return stateToCheck[i, j];
+                if (stateToCheck[j, i] != null)
+                {
+                    if (stateToCheck[j, i] == stateToCheck[j + 1, i])
+                        c++;
+                    if (c == 2)
+                        return stateToCheck[j, i];
+                }
             }
             c = 0;
         }
@@ -176,23 +231,28 @@ public class GameLogic : MonoBehaviour
         // check diagonal1
         for (int i = 0; i < 2; i++)
         {
-            if (stateToCheck[i, i] != null && stateToCheck[i, i] == stateToCheck[i + 1, i + 1])
-                c++;
-            if (c == 2)
-                return stateToCheck[i, i];
+            if (stateToCheck[i, i] != null)
+            {
+                if (stateToCheck[i, i] == stateToCheck[i + 1, i + 1])
+                    c++;
+                if (c == 2)
+                    return stateToCheck[i, i];
+            }
         }
-
         c = 0;
 
         // check diagonal2
         int column = 0;
         for (int i = 2; i > 0; i--)
         {
-            if (stateToCheck[i, column] != null && stateToCheck[i, column] == stateToCheck[i - 1, column + 1])
-                c++;
-            if (c == 2)
-                return stateToCheck[i, i];
-            column++;
+            if (stateToCheck[i, column] != null)
+            {
+                if (stateToCheck[i, column] == stateToCheck[i - 1, column + 1])
+                    c++;
+                if (c == 2)
+                    return stateToCheck[i, column];
+                column++;
+            }
         }
 
         // check draw
@@ -201,10 +261,21 @@ public class GameLogic : MonoBehaviour
             for (int j = 0; j < 3; j++)
             {
                 if (board[i, j] == null)
-                    return "Continue";
+                    return "";
             }
         }
 
-        return "Draw";
+        return "D";
+    }
+    [ContextMenu("Print")]
+    public void PrintMatrix()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                print($"{i} {j} {board[i, j]}");
+            }
+        }
     }
 }
