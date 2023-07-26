8b3df886ee0b27a02ccf74124788033e83ae759a
NicolaInvernizzi <nicola.invernizzi99@gmail.com>
2023-07-26 13:05:13 +0200

    Boid - Working on final version

--- Studying/Assets/Scripts/AI/Boid/Fish.cs
+++ Studying/Assets/Scripts/AI/Boid/Fish.cs
@@ -3,14 +3,13 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    [SerializeField] Transform debug;
+    [SerializeField] float maxSpeed = 2f;
     [SerializeField] float speed = 2f;
     [SerializeField] float coesionRange = 2f;
     [SerializeField] float separationRange = 2f;
     [SerializeField] float obstacleRange = 2f;
-    [SerializeField] float repulsion = 50f;
     [SerializeField] LayerMask obstacleMask;
-    [SerializeField] bool d;
+    public float repulsion = 30f;
     Vector3 direction;
     Vector3 velocity;
 
@@ -20,98 +19,90 @@ public class Fish : MonoBehaviour
     }
     void Update()
     {
-        Coesion();
-        Separation();
-        Align();
-        AvoidObstacles();
+        direction += Coesion();
+        direction += Separation();
+        direction += Align();
+        direction += AvoidObstacles();
 
-        velocity = direction.normalized * speed * Time.deltaTime;
-        transform.forward = velocity;
-
-        transform.position += velocity;
+        velocity = direction.normalized * Mathf.Min(direction.magnitude, maxSpeed);
+        transform.LookAt(transform.position + velocity);
+        transform.position += velocity * Time.deltaTime;
     }
 
-    void AvoidObstacles()
+    Vector3 AvoidObstacles()
     {
+        Vector3 totRepulsion = Vector3.zero;
+
         Collider[] obstacles = Physics.OverlapSphere(transform.position, obstacleRange, obstacleMask);
         if (obstacles.Length == 0f)
-            return;
+            return totRepulsion;
 
-        Vector3 totRepulsion = Vector3.zero;
         foreach(Collider obstacle in obstacles)
         {
-            totRepulsion += (transform.position - obstacle.ClosestPoint(transform.position)).normalized 
-                * Remap(0f, obstacleRange, Vector3.Distance(transform.position, obstacle.ClosestPoint(transform.position)), 1f, 0f);
+            totRepulsion += transform.position - obstacle.ClosestPoint(transform.position);
         }
-        if (d)
-        {
-            Debug.DrawRay(new Vector3(debug.position.x, 0f, debug.position.z - 5), Vector3.right * repulsion, Color.blue);
-        }
-        direction += totRepulsion.normalized * repulsion;
+
+        return totRepulsion.normalized * repulsion;
     }
-    void Coesion()
+    Vector3 Coesion()
     {
+        Vector3 totCoesion = Vector3.zero;
+
         GameObject[] neighbors = GameObject.FindGameObjectsWithTag("Boid").Where(
             n => n != gameObject && Vector3.Distance(n.transform.position, transform.position) <= coesionRange).ToArray();
 
-        if (neighbors.Length == 0f)
-            return;
+        if (neighbors.Length <= 0f)
+            return totCoesion;
 
-        Vector3 totCoesion = Vector3.zero;
         foreach(GameObject neighbor in neighbors)
         {
             totCoesion += neighbor.transform.position;
         }
         totCoesion /= neighbors.Length;
 
-        if (d)
-        {
-            Debug.DrawRay(new Vector3(debug.position.x, 0f, debug.position.z - 6), Vector3.right * (totCoesion - transform.position).magnitude, Color.red);
-        }
-        direction += (totCoesion - transform.position);
+        return totCoesion - transform.position;
     }
-    void Separation()
+    Vector3 Separation()
     {
-        GameObject[] neighbors = GameObject.FindGameObjectsWithTag("Boid").Where(
-            n => n != gameObject && Vector3.Distance(n.transform.position, transform.position) <= separationRange).ToArray();
-
-        if (neighbors.Length == 0f)
-            return;
-
         Vector3 totSeparation = Vector3.zero;
-        foreach (GameObject neighbor in neighbors)
-        {
-            totSeparation += (neighbor.transform.position - transform.position)
-                 * Remap(0f, obstacleRange, Vector3.Distance(transform.position, neighbor.transform.position), 1f, 0f);
-        }
 
-        if (d)
-        {
-            Debug.DrawRay(new Vector3(debug.position.x, 0f, debug.position.z - 7), Vector3.right * totSeparation.magnitude, Color.green);
-        }
-        direction += totSeparation;
-    }
-    void Align()
-    {
         GameObject[] neighbors = GameObject.FindGameObjectsWithTag("Boid").Where(
             n => n != gameObject && Vector3.Distance(n.transform.position, transform.position) <= separationRange).ToArray();
 
         if (neighbors.Length == 0f)
-            return;
+            return totSeparation;
 
-        Vector3 totAlign = Vector3.zero;
+        //foreach (GameObject neighbor in neighbors)
+        //{
+        //    totSeparation += (transform.position - neighbor.transform.position);
+        //}
+
+        //direction += totSeparation;
         foreach (GameObject neighbor in neighbors)
         {
-            totAlign += neighbor.transform.forward 
-                * Remap(0f, obstacleRange, Vector3.Distance(transform.position, neighbor.transform.position), 1f, 0f);
+            totSeparation += neighbor.transform.position;
+        }
+        totSeparation /= neighbors.Length;
+
+        return transform.position - totSeparation;
+    }
+    Vector3 Align()
+    {
+        Vector3 totAlign = Vector3.zero;
+
+        GameObject[] neighbors = GameObject.FindGameObjectsWithTag("Boid").Where(
+            n => n != gameObject && Vector3.Distance(n.transform.position, transform.position) <= separationRange).ToArray();
+
+        if (neighbors.Length == 0f)
+            return totAlign;
+
+        foreach (GameObject neighbor in neighbors)
+        {
+            totAlign += neighbor.transform.forward;
         }
         totAlign /= neighbors.Length;
 
-        if (d)
-        {
-            Debug.DrawRay(new Vector3(debug.position.x, 0f, debug.position.z - 8), Vector3.right * totAlign.magnitude, Color.yellow);
-        }
-        direction += totAlign;
+         return totAlign;
     }
     float Remap(float min_A, float max_A, float A, float min_B, float max_B)
     {
